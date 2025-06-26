a3adc5a80a5549f0468bf574794949a6980c9369
gahyeon <keh5667@gmail.com>
2025-06-26 13:30:06 +0900

    Feat: 공통 컴포넌트 구현 - Input, FlexBox, Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import './App.css'
+import Alert from './components/shared/Alert'
 import Button from './components/shared/Button'
+import Input from './components/shared/Input'
 import Text from './components/shared/Text'
+import TextField from './components/shared/TextField'
+import { useAlertContext } from './context/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
   return (
     <div>
       <Text color="blue">t1</Text>
       <Button>button</Button>
+      <Input placeholder="login" />
+
+      <TextField label="아이디" />
+
+      {/* <Alert
+        title="알림"
+        onButtonClick={() => {}}
+        open={true}
+        description="알럿"
+      ></Alert> */}
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료',
+            description: 'dd',
+            onButtonClick: () => {},
+          })
+        }}
+      >
+        알럿 오픈
+      </Button>
     </div>
   )
 }
